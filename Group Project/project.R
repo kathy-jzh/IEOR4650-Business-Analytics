a1bdde1e3fbb2487c5eab3498a7d6732a111ecb1
Zehui Jin <37889165+kathy-jzh@users.noreply.github.com>
2019-04-21 11:15:00 -0400

    add decision tree

--- Group Project/project.R
+++ Group Project/project.R
@@ -26,13 +26,16 @@ model_col_list = c("artist.hotness", "duration",
 music = music[,model_col_list]
 music$key = as.factor(music$key)
 music$mode = as.factor(music$mode)
-music$grouped_terms = str(music$grouped_terms)
+music$grouped_terms = as.factor(music$grouped_terms)
 music$time_signature = as.factor(music$time_signature)
 music$decade = as.factor(music$decade)
-music$label = str(music$label)
+music$label = as.factor(music$label)
+
+plot(music$artist.hotness, music$song.hotness)
 
 attach(music)
 
+# split data into three classes according to artist hotness
 high_class = music[which(artist.hotness>0.7),]
 medium_class = music[which(artist.hotness>0.2 & artist.hotness<0.7),]
 small_class = music[which(artist.hotness<0.2),]
@@ -41,18 +44,55 @@ plot(high_class$artist.hotness, high_class$song.hotness)
 cor(high_class$artist.hotness, high_class$song.hotness)
 plot(medium_class$artist.hotness, medium_class$song.hotness)
 cor(medium_class$artist.hotness, medium_class$song.hotness)
+plot(small_class$artist.hotness, small_class$song.hotness)
+cor(small_class$artist.hotness, small_class$song.hotness)
+
+# train, val, test set split
+train = sample(nrow(music), as.integer(nrow(music)*0.7))
+idxNotTrain <- which(! 1:nrow(music) %in% train )
+val = sample(idxNotTrain,as.integer(length(idxNotTrain)*0.5))
+test = idxNotTrain[which(! idxNotTrain %in% val)]
+length(train); length(val); length(test)
+
+## decision tree
+library(tree)
+library(ISLR)
+music_tree = tree(song.hotness~., music[train, ])
+summary(music_tree)
+cv.music=cv.tree(music_tree)
+prune.music=prune.tree(music_tree,best=5)
+plot(prune.music)
+text(prune.music,pretty=0)
+
+yhat=predict(music_tree,newdata=music[val,])
+ytrue=music[val,"song.hotness"]
+plot(ytrue,yhat)
+abline(0,1)
+mean((yhat-ytrue)^2)
+
+# clustering
 
 
-##########
-pr.out=prcomp(music[-1],scale=T)
-summary(pr.out)
-pr.var=pr.out$sdev^2
-pve=pr.var/sum(pr.var)
-pve
-plot(cumsum (pve), xlab=" Principal Component ", ylab ="Cumulative Proportion of Variance Explained ", 
-     ylim=c(0,1), type="b")
-############## tetst #############
 
-linreg = lm(song.hotness~artist.hotness, data=data)
-summary(linreg)
-cor(song.hotness,artist.hotness)
+# create dummy variables
+library(dummies)
+music = cbind(music, dummy(music$key))
+music = cbind(music, dummy(music$mode))
+music = cbind(music, dummy(music$grouped_terms))
+music = cbind(music, dummy(music$time_signature))
+music = cbind(music, dummy(music$decade))
+music = cbind(music, dummy(music$label))
+
+########## PCA
+# pr.out=prcomp(music[-1],scale=T)
+# summary(pr.out)
+# pr.var=pr.out$sdev^2
+# pve=pr.var/sum(pr.var)
+# pve
+# plot(cumsum (pve), xlab=" Principal Component ", ylab ="Cumulative Proportion of Variance Explained ", 
+#     ylim=c(0,1), type="b")
+
+############## test #############
+# linreg = lm(song.hotness~artist.hotness, data=data)
+# summary(linreg)
+# cor(song.hotness,artist.hotness)
